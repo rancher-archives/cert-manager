99b53ee6718ec996a30b6e2595e286869027a67c
Geert van Horrik <GeertvanHorrik@users.noreply.github.com>
2024-06-18 09:23:55 +0200

    Update config.go

--- test/e2e/framework/config/config.go
+++ test/e2e/framework/config/config.go
@@ -63,7 +63,7 @@ func (c *Config) Validate() error {
 func (c *Config) AddFlags(fs *flag.FlagSet) {
 	// Kubernetes API server config
 	fs.StringVar(&c.KubeConfig, "kubernetes-config", os.Getenv(clientcmd.RecommendedConfigPathEnvVar), "Path to config containing embedded authinfo for kubernetes. Default value is from environment variable "+clientcmd.RecommendedConfigPathEnvVar)
-	fs.StringVar(&c.KubeContext, "kubernetes-context", "", "config context to use for kuberentes. If unset, will use value from 'current-context'")
+	fs.StringVar(&c.KubeContext, "kubernetes-context", "", "config context to use for kubernetes. If unset, will use value from 'current-context'")
 	fs.BoolVar(&c.Cleanup, "cleanup", true, "If true, addons will be cleaned up both before and after provisioning")
 
 	// TODO: get rid of this variable by bundling required files as part of test suite
